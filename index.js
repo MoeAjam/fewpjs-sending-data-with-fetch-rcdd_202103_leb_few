97dde8d4f87118cd04f20bf06f76da4ba0eea28e
Mahmoud Ajam <mahmoud.aajam@gmail.com>
2021-05-03 18:12:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Add your code here
+function submitData(name, email){
+  const option = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({name,email})
+  }
+  return fetch("http://localhost:3000/users", option);
+}
